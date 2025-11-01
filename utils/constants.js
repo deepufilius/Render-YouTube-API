158e4e9081bffb47a132f4fc5f1625b715056e06
deepu filius <deepuadari@gmail.com>
2025-11-01 12:03:23 +0530

    add env file

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,4 @@
+
 export const HAMBURGER_ICON = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png";
 
 export const LOGO_URL= "https://www.gstatic.com/youtube/img/promos/dc5f74641537e0e7c0b4de98b2c2a88213edd936ab0b8d14ba1bfa13d4a08f02_129x56.webp";
@@ -6,7 +7,7 @@ export const USER_ICON = "https://www.iconpacks.net/icons/2/free-user-icon-3296-
 
 export const TAGS = ["All", "Computer Programming","Telugu Cinema","Spice","Restaurants","History","Display Devices","Podcasts","Desktop Computers"]
 
-export const API_KEY = "AIzaSyCrbW_l-I26mP_K5aIMtXW76SkXfm6wLVc";
+export const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+API_KEY;
 
